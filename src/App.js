734652254ba9e214d6d223bd82190f6a11d17082
Ryan Santos <ryansantos86@gmail.com>
2019-01-19 17:23:10 -0800

    commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,52 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styled from 'styled-components';
+import { SLIDES } from './constants';
 
 class App extends Component {
+  state = {
+    closed: false
+  };
+
+  slideLeft = () => {};
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <AppWrap>
+        <SlideContainer>
+          <SlideImg
+            src={SLIDES[0].imgUrl}
+            alt="musician"
+            closed={this.state.closed}
+          />
+        </SlideContainer>
+        <button onClick={() => this.setState({ closed: !this.state.closed })}>
+          slide
+        </button>
+      </AppWrap>
     );
   }
 }
+
+const AppWrap = styled.div`
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+`;
+
+const SlideContainer = styled.div`
+  height: 60%;
+  width: 80%;
+`;
+
+const SlideImg = styled.img`
+  height: 100%;
+  width: ${({ closed }) => (closed ? '0' : '100%')};
+  overflow-y: hidden;
+  transition-property: all;
+  transition-duration: 0.5s;
+  transition-timing-function: cubic-bezier(0, 1, 0.5, 1);
+`;
 
 export default App;
---
+++ src/constants.js
@@ -0,0 +1,17 @@
+const SLIDES = [
+  {
+    desc: '',
+    imgUrl: 'https://i.postimg.cc/L809KRth/neonbrand-265876-unsplash-scaled.jpg'
+  },
+  {
+    desc: '',
+    imgUrl:
+      'https://i.postimg.cc/rwpVPztv/glenn-van-de-wiel-1168903-unsplash-scaled.jpg'
+  },
+  {
+    desc: '',
+    imgUrl: 'https://i.postimg.cc/VNrYzTgg/neonbrand-350408-unsplash-scaled.jpg'
+  }
+];
+
+export { SLIDES };
